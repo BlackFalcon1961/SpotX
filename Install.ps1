0d9bcf93fb97addae58e7f40debab8f7c5a00ed6
BlackFalcon1961 <60007836+BlackFalcon1961@users.noreply.github.com>
2023-08-21 18:21:20 +0200

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -425,7 +425,7 @@ function downloadSp() {
 
     Import-Module BitsTransfer
         
-    $web_Url = "https://download.scdn.co/Spotify$onlineFullSetup.exe"
+    $web_Url = "https://upgrade.scdn.co/upgrade/client/win32-x86/spotify_installer$online.exe"
     $local_Url = "$PWD\SpotifySetup.exe" 
     $web_name_file = "SpotifySetup.exe"
 
